a33e0486a30f393edc730a253a78e23ed2ffa17e
Yulong Tan <swifties@uw.edu>
2015-07-27 21:34:14 -0700

    linked second activity

--- app/src/main/java/yulongproductions/com/thelabyrinth/model/Story.java
+++ app/src/main/java/yulongproductions/com/thelabyrinth/model/Story.java
@@ -1,4 +1,5 @@
 package yulongproductions.com.thelabyrinth.model;
+import yulongproductions.com.thelabyrinth.R;
 
 /**
  * Created by Yulong on 7/25/2015.
@@ -8,5 +9,16 @@ public class Story {
 
     public Story() {
         this.pages = new Page[100];
+
+        this.pages[0] = new Page (
+                R.drawable.labyrinth_start_image,
+                "The dim light flickers as you start to regain your vision. Your mind is boggled and you feel heavy. You get up and groan to yourself \"Where am I? I don't recognize this place...\" You try to look at your surroundings but you find that you're in an enclosed room with only dimly lit passageway in front of you. Not only that, but written in blood on the walls was a single name: %1$s",
+                new Choice("Head towards the passageway", 1),
+                new Choice("Stay in the room", 2)
+        );
+    }
+
+    public Page getPage(int pageNumber) {
+        return this.pages[pageNumber];
     }
 }
---
+++ app/src/main/java/yulongproductions/com/thelabyrinth/ui/StoryActivity.java
@@ -0,0 +1,92 @@
+package yulongproductions.com.thelabyrinth.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import yulongproductions.com.thelabyrinth.R;
+import yulongproductions.com.thelabyrinth.model.Page;
+import yulongproductions.com.thelabyrinth.model.Story;
+
+public class StoryActivity extends Activity {
+
+    // Fields (Member Variables)
+    private Story mStory = new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
+    private String mName;
+    private Page mCurrentPage;
+
+    // Class constants
+    public static final String TAG = StoryActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        Intent i = getIntent();
+        mName = i.getStringExtra(getString(R.string.name));
+
+        if (mName == null) {
+            mName = "Pan";
+        }
+        Log.d(TAG, mName);
+
+        mImageView = (ImageView) findViewById(R.id.storyImageView);
+        mTextView = (TextView) findViewById(R.id.storyTextView);
+        mChoice1 = (Button) findViewById(R.id.choiceButton1);
+        mChoice2 = (Button) findViewById(R.id.choiceButton2);
+
+        loadPage(0);
+    }
+
+    private void loadPage(int choice) {
+        mCurrentPage = mStory.getPage(choice);
+        Drawable drawable = getResources().getDrawable(mCurrentPage.getImageId());
+
+        mImageView.setImageDrawable(drawable);
+        String pageText = mCurrentPage.getText();
+        pageText = String.format(pageText, mName);
+        mTextView.setText(pageText);
+
+        if (mCurrentPage.isFinal()) {
+            mChoice1.setVisibility(View.INVISIBLE);
+            mChoice2.setText("PLAY AGAIN");
+            mChoice2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+        } else {
+            mChoice1.setText(mCurrentPage.getChoice1().getText());
+            mChoice2.setText(mCurrentPage.getChoice2().getText());
+
+            mChoice1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    loadPage(mCurrentPage.getChoice1().getNextPage());
+                }
+            });
+
+            mChoice2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    loadPage(mCurrentPage.getChoice1().getNextPage());
+                }
+            });
+        }
+    }
+}
