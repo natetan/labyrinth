5448c58a8c5ce58eef99266a2fbd171b92eebf27
Yulong Tan <swifties@uw.edu>
2015-09-04 22:08:45 -0700

    added the images for pages 19 and 20

--- app/src/main/java/yulongproductions/com/thelabyrinth/model/Story.java
+++ app/src/main/java/yulongproductions/com/thelabyrinth/model/Story.java
@@ -159,7 +159,7 @@ public class Story {
         );
 
         this.pages[19] = new Page (
-                0, // Picture of a dark cave in the woods
+                R.drawable.labryinth_cave,
                 R.raw.dead_silence,
                 "That was the strangest thing you've seen so far and you start running past the clearing in fear for your life. Making the journey through the woods was tough, since it was dark and the terrain of the forest floor was rough. That annoying incessant buzzing sound soon dissipates as you get further away. Out of breath, you stumble upon a cave with a small opening.",
                 new Choice("Explore the cave", 24),
@@ -167,7 +167,7 @@ public class Story {
         );
 
         this.pages[20] = new Page (
-                0, // a visual splash to make users believe it was the fish thrown into the river again
+                R.drawable.labryinth_cave,
                 R.raw.dead_silence,
                 "Although the fish did attack and try to drown you, you feel a pang of sympathy for it. It was just in its nature to pull off a stunt like that so you decide to let it live. You pick it up with both hands (it was surprisingly heavy) and toss it back into the river, hoping it gets back to safety. You check your surroundings to see that you can follow the river or go up the hill into the woods",
                 new Choice("Follow the river", 26),
